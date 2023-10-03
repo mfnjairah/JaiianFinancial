47896e7601e3c751505aba07418a1e1594a56618
Mark Ian Amado <amado.markian@gmail.com>
2023-10-03 23:10:48 +0800

    Add UI for Dashboard of User

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Header from "./components/Header";
 import Deposit from "./pages/Deposit";
 import Withdraw from "./pages/Withdraw";
 import Transfer from "./pages/Transfer";
+import DashboardUser from "./pages/DashboardUser";
 
 // Utils
 import PrivateRoute from "./utils/PrivateRoute";
