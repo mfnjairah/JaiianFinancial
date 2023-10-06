058f8fe7b52e197e54c4e2b37e42725fa1640c10
Mark Ian Amado <amado.markian@gmail.com>
2023-10-06 16:36:25 +0800

    Finish Budget Application

--- frontend/src/pages/BudgetApp.jsx
+++ frontend/src/pages/BudgetApp.jsx
@@ -1,15 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./BudgetApp.css"
 import { BsFillPieChartFill } from "react-icons/bs";
 import { MdDelete } from "react-icons/md";
 
 const BudgetApp = ({ users, currentUser }) => {
+    const storedUsers = JSON.parse(localStorage.getItem("userz")) || users;
+    const LoggedInUser = storedUsers.find(
+            (user) => user.name == currentUser.name)
 
     const [amount, setAmount] = useState("");
     const [name, setName] = useState('');
-    const [item, setItem] = useState(currentUser.budgetApplication);
+    const [item, setItem] = useState(LoggedInUser.budgetApplication);
+    const [balance, setBalance] = useState(LoggedInUser.accountBalance);
+
+    const ids = item.map(id => {
+        return id.ID;
+    });
+
+    const maxID = Math.max(...ids) + 1;
 
     
+
+    useEffect(() => {
+        const storedUsers = JSON.parse(localStorage.getItem("userz")) || users;
+        const userIndex = storedUsers.findIndex((user) => user.name === currentUser.name);
+        const updatedUsers = [...storedUsers];
+        updatedUsers[userIndex] = {
+            ...updatedUsers[userIndex],
+            budgetApplication: item
+        };
+
+        updatedUsers[userIndex] = {
+            ...updatedUsers[userIndex],
+            accountBalance: balance
+        };
+
+        localStorage.setItem("userz", JSON.stringify(updatedUsers));
+        const storedUsers1 = JSON.parse(localStorage.getItem("userz")) || users;
+        console.log(storedUsers1);
+    }, [item]
+    );
+
+    const HandleOnAdd = (e) => {
+        e.preventDefault();
+    
+            setItem([
+                    ...item,
+                    { ID: maxID, ItemName: name, amount: amount }
+            ]);
+
+            const updatedBalance = balance - amount;
+            setBalance(updatedBalance)
+    }
+    
+
+
     return (
         <div className="main-container">
             <div className="budget-container">
@@ -26,24 +71,13 @@ const BudgetApp = ({ users, currentUser }) => {
                     onChange={e => setAmount(e.target.value)}
                     placeholder="Amount"
                 />
-                <button className= "add-item-button" onClick={() => {
-                    let ids = item.map(id => {
-                        return id.ID;
-                    });
-                
-                    let maxID = Math.max(...ids) + 1;
-                    setItem([
-                    ...item,
-                    { ID: maxID, ItemName: name, amount: amount }
-                    ]);
-
-                    console.log(currentUser.budgetApplication)
-                    
-                }}>Add</button>
-
+                <button className= "add-item-button" onClick={HandleOnAdd}>Add</button>
 
                 <table className="budget-table">
                     <thead>
+                        <tr className="total-balance-row">
+                            <th colSpan = "3" className="curr-bal-cell">Current Balance: {balance}</th>
+                        </tr>
                         <tr className="budget-table-row">
                             <th>Item Name</th>
                             <th>Amount</th>
@@ -57,10 +91,11 @@ const BudgetApp = ({ users, currentUser }) => {
                                 <td className="budget-data">P {itim.amount}.00</td>
                                 <td className="budget-data">
                                 <button onClick={() => {
+                                    setBalance(Number(balance) + Number(itim.amount))
                                     setItem(item.filter(a => a.ID !== itim.ID));
                                 }
-                                }><MdDelete />
-                                    </button>
+                                }
+                                ><MdDelete /></button>
                                 </td>
                             </tr>
                         ))}
@@ -73,3 +108,4 @@ const BudgetApp = ({ users, currentUser }) => {
 };
 
 export default BudgetApp;
+
