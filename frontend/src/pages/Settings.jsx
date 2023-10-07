297d262fa1ce24eb5d2abc642c87bff36be2ae0f
Mark Ian Amado <amado.markian@gmail.com>
2023-10-08 02:58:26 +0800

    Add the change Password Feature in the Settings

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -6,11 +6,56 @@ import { useState } from "react";
 
 const Settings = ({ users, currentUser }) => {
 
-const [buttonClicked, setButtonClicked] = useState('');
+const storedUsers = JSON.parse(localStorage.getItem("userz")) || users;
+const LoggedInUser = storedUsers.find((user) => user.name == currentUser.name);
 
-const originalString = currentUser.password;
+const [buttonClicked, setButtonClicked] = useState('');
+const [tabPass, setTabPass] = useState(LoggedInUser.password);
+
+const originalString = tabPass;
 const convertedString = originalString.split('').map(() => '*').join('');
 
+const [oldPass, setOldPass] = useState('');
+const [insPass, setInsPass] = useState('');
+const [changeNewPass, setChangeNewPass] = useState('');
+
+
+const changePassSubmit = (e) => {
+    e.preventDefault();
+    const userIndex = storedUsers.findIndex((user) => user.accountNumber == currentUser.accountNumber);
+    
+    if(oldPass != insPass && oldPass != changeNewPass) {
+        if (oldPass === LoggedInUser.password) {
+            if(insPass === changeNewPass){
+                const updatedUsers = [...storedUsers];
+                const newPass = insPass;
+
+                updatedUsers[userIndex] = {
+                    ...updatedUsers[userIndex],
+                    password: newPass
+                };
+
+
+                setTabPass(newPass.split('').map(() => '*').join(''))
+                setOldPass('')
+                setInsPass('')
+                setChangeNewPass('')
+
+                localStorage.setItem("userz", JSON.stringify(updatedUsers));
+                alert(`Transaction successful.`);
+            }
+            else {
+                alert("New Password Doesn't Match.")
+            }
+        }
+        else {
+            alert("Wrong Credentials.")
+        }
+    }
+    else {
+        alert("Old Password Can't Be Your New Password.")
+    }
+}
 
   return (
     <div className="register-div">
@@ -31,21 +76,21 @@ const convertedString = originalString.split('').map(() => '*').join('');
                         <tr>
                             <td className="table-desc">Full Name</td>
                             <td className= "table-value-name">
-                                <span className="table-value-long">{currentUser.name}</span>
+                                <span className="table-value-long">{LoggedInUser.name}</span>
                             </td>
                         </tr>
                         <tr>
                             <td className="table-desc">Email</td>
                             <td className= "table-value">
-                                <span className="table-value-long">{currentUser.email}</span>
+                                <span className="table-value-long">{LoggedInUser.email}</span>
                                 <span className="table-button" 
                                     onClick={(e) => {
                                     e.preventDefault();
-                                    if (buttonClicked === "") {
-                                    setButtonClicked("Email");
+                                    if (buttonClicked === "Email") {
+                                    setButtonClicked("");
                                     }
-                                    else if (buttonClicked === "Email"){
-                                        setButtonClicked("");
+                                    else {
+                                        setButtonClicked("Email");
                                     }
                                 }}>
                                 <AiFillEdit className="trans-icon"/></span>
@@ -54,15 +99,15 @@ const convertedString = originalString.split('').map(() => '*').join('');
                         <tr>
                             <td className="table-desc">Username</td>
                             <td className= "table-value">
-                                <span className="table-value-long">{currentUser.userName}</span>
+                                <span className="table-value-long">{LoggedInUser.userName}</span>
                                 <span className="table-button" 
                                     onClick={(e) => {
                                     e.preventDefault();
-                                    if (buttonClicked === "") {
-                                    setButtonClicked("Username");
+                                    if (buttonClicked === "Username") {
+                                    setButtonClicked("");
                                     }
-                                    else if (buttonClicked === "Username"){
-                                        setButtonClicked("");
+                                    else {
+                                        setButtonClicked("Username");
                                     }
                                 }}>
                                 <AiFillEdit className="trans-icon"/></span>
@@ -75,11 +120,11 @@ const convertedString = originalString.split('').map(() => '*').join('');
                                 <span className="table-button" 
                                     onClick={(e) => {
                                     e.preventDefault();
-                                    if (buttonClicked === "") {
-                                    setButtonClicked("Password");
-                                    }
-                                    else if (buttonClicked === "Password"){
+                                    if (buttonClicked === "Password") {
                                         setButtonClicked("");
+                                    }
+                                    else {
+                                        setButtonClicked("Password");
                                     }
                                 }}>
                                 <AiFillEdit className="trans-icon"/></span>
@@ -122,17 +167,37 @@ const convertedString = originalString.split('').map(() => '*').join('');
                 {buttonClicked === "Password" && (
                     <div className="config-container">
                         <h1>Username Configuration</h1>
-                        <div className="settings-form-container">
-                            <input className = "input-item-name"
-                            placeholder="Insert Old Password"
-                            />
-                            <input className = "input-item-name"
-                            placeholder="Insert New Password"
-                            />
-                            <input className = "input-item-name"
-                            placeholder="Confirm New Password"
-                            />
-                            <button className= "add-item-button">Submit</button>
+                        <div>
+                            <form className="settings-form-container" onSubmit={changePassSubmit}>
+                                <input className = "input-item-name"
+                                value={oldPass}
+                                onChange={e => {
+                                    setOldPass(e.target.value)
+                                }}
+                                placeholder="Insert Old Password"
+                                type="password"
+                                />
+
+                                <input className = "input-item-name"
+                                placeholder="Insert New Password"
+                                value={insPass}
+                                onChange={e => {
+                                    setInsPass(e.target.value)
+                                }}
+                                type="password"
+                                />
+
+                                <input className = "input-item-name"
+                                placeholder="Confirm New Password"
+                                value={changeNewPass}
+                                onChange={e => {
+                                    setChangeNewPass(e.target.value)
+                                }}
+                                type="password"
+                                />
+
+                                <button className= "add-item-button">Submit</button>
+                            </form>
                         </div>
                     </div>
                 )}
